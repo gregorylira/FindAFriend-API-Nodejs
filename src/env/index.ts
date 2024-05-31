57c50e13e8ff6021a447514082c817b9cd47c37a
gregorylira <gregory.lira007@gmail.com>
2024-05-31 09:24:56 -0300

    initial config server to fastify

---
+++ src/env/index.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import z from "zod";
+
+const envSchema = z.object({
+  PORT: z.string().default("3000"),
+  NODE_ENV: z.string().default("development"),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.log("Invalid environment variables", _env.error.format());
+
+  throw new Error("Invalid environment variables");
+}
